b1fb26f3a6e839f951e2b9cbe1cea6ce7f422a39
ThaiAn31 <99635907+ThaiAn31@users.noreply.github.com>
2024-01-26 10:01:51 +0700

    update

--- node-slack/index.js
+++ node-slack/index.js
@@ -1,58 +1,86 @@
-const { App, ExpressReceiver } = require('@slack/bolt');
+const { App } = require('@slack/bolt');
 const dotenv = require('dotenv');
-const express = require('express');
 
-dotenv.config({ path: 'slack.env' });
-// console.log('SLACK_SIGNING_SECRET:', process.env.SLACK_SIGNING_SECRET);
+// dotenv.config({ path: 'slack.env' });
 
-// const expressReceiver = new ExpressReceiver({ signingSecret: process.env.SLACK_SIGNING_SECRET });
+const SLACK_BOT_TOKEN = "xoxb-6537107037488-6514224563171-lIwJdxhkK3fosH0Pbrt2VZXG";
+const SLACK_SIGNING_SECRET = "52272410b380a5b24aebde64ab9948cb";
+const SLACK_APP_TOKEN = "xapp-1-A06EPP2EYP9-6537562262944-bbee3af1b582955687773ead9e77f98ed2599d727a436bd99afe9b066538d777";
+const SLACK_CHANNEL = "C06FGUAP741";
 const app = new App({
-    signingSecret: process.env.SLACK_SIGNING_SECRET,
-    token: process.env.SLACK_BOT_TOKEN,
-    // receiver: expressReceiver,
+  token: SLACK_BOT_TOKEN,
+  signingSecret: SLACK_SIGNING_SECRET,
+  socketMode: true,
+  appToken: SLACK_APP_TOKEN,
+  // Socket Mode doesn't listen on a port, but in case you want your app to respond to OAuth,
+  // you still need to listen on some port!
+  port: process.env.PORT || 3000
 });
 
-// // Xử lý tin nhắn từ Slack
+// Listens to incoming messages that contain "hello"
+// app.message('hello', async ({ message, say }) => {
+//   console.log(message);
+//   // say() sends a message to the channel where the event was triggered
+//   try {
+//     await say(`Hey there <@${message.user}>!`);
+//   } catch (error) {
+//     console.error('Error responding to message:', error);
+//   }
+// });
+
 // app.message(async ({ message, say }) => {
-//     await say(`Xin chào, bạn vừa nói: ${message.text}`);
-//     console.log('Nội dung tin nhắn:', message.text);
+//   try {
+//     // Lấy lịch sử tin nhắn từ kênh
+//     const history = await app.client.conversations.history({
+//       token: SLACK_BOT_TOKEN,
+//       channel: SLACK_CHANNEL,
+//     });
+    
+//     // Xử lý và hiển thị tin nhắn theo cách bạn muốn
+//     const messages = history.messages;
+//     messages.forEach((msg) => {
+//       say(`${msg.user}: ${msg.text}`);
+//       console.log(history.messages);
+//     });
+//   } catch (error) {
+//     console.error('Error:', error);
+//   }
 // });
 
-// Xử lý yêu cầu GET đến root của Express
-// expressReceiver.app.get('/', (req, res) => {
-//     // Trả về mã HTML trực tiếp từ đoạn mã JavaScript
-//     res.send(`
-//         <!DOCTYPE html>
-//         <html lang="en">
-//         <head>
-//             <meta charset="UTF-8">
-//             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//             <title>Your Slack App</title>
-//         </head>
-//         <body>
-//         <a href="https://slack.com/oauth/v2/authorize?scope=chat:write&client_id=6514122443442.6514510899139">
-//         Log in to Slack
-//         </a>
-//         </body>
-//         </html>
-//     `);
+app.message(async ({ message, say }) => {
+  console.log(message);
+  
+  // Send a message to a channel
+  try {
+      const result = await app.client.chat.postMessage({
+
+          token: SLACK_BOT_TOKEN,
+          channel: SLACK_CHANNEL, // Replace with your channel ID
+          text: 'Toi la An!',
+      });
+      console.log('Message sent successfully:', result);
+  } catch (error) {
+      console.error('Error sending message:', error);
+  }
+});
+
+
+// app.message(async ({ message, say }) => {
+//   await say(`Xin chào, bạn vừa nói: ${message.text}`);
+//   console.log('Nội dung tin nhắn:', message.text);
 // });
 
-// Kết nối đến Slack webhook và gửi tin nhắn
-const axios = require('axios');
-const webhookUrl = 'https://hooks.slack.com/services/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';
-const message = 'Hello, this is a test message!';
-
-axios.post(webhookUrl, { text: message })
-    .then(response => {
-        console.log('Message sent successfully:', response.data);
-    })
-    .catch(error => {
-        console.error('Error sending message:', error);
-    });
-
+// Handle errors
+app.error((error) => {
+  console.error('Error:', error);
+});
 
 (async () => {
-    await app.start(process.env.PORT || 3200);
-    console.log('App is running!');
+  // Start your app
+  try {
+    await app.start();
+    console.log('⚡️ Bolt app is running!');
+  } catch (error) {
+    console.error('Error starting the app:', error);
+  }
 })();
